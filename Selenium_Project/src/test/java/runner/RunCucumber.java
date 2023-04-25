ab5b6d6543ac8fc5686ee2f74e68606d49f9c254
abhi7uday <abhi7uday@gmail.com>
2023-04-25 17:37:33 +0530

    BBD report

---
+++ Selenium_Project/src/test/java/runner/RunCucumber.java
@@ -0,0 +1,9 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features ="src/test/resources/features", glue = {"steps"}, plugin = {"html:target/cucumber-report/cucumber-report.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class RunCucumber extends AbstractTestNGCucumberTests{
+
+}
